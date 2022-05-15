2b05ca9d6576572c76d350de345282bd36897b50
Ryan Esmores <ryanesmores07@gmail.com>
2022-05-15 23:52:37 +0900

    first commit

---
+++ components/events/EventList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import EventItem from "./EventItem";
+
+const EventList = (props) => {
+  const { items } = props;
+  return (
+    <ul>
+      {items.map((event) => {
+        return (
+          <EventItem
+            key={event.id}
+            id={event.id}
+            title={event.title}
+            location={event.location}
+            date={event.date}
+            image={event.image}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default EventList;
